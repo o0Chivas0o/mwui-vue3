3fb263525e22c802ea7efefcf9715c31cca21e4c
Lee <326656669@qq.com>
2020-07-29 21:17:32 +0800

    feat : init project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Switch></Switch>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Switch from './components/Switch'
+
+export default {
+  name: 'App',
+  components: {
+    Switch
+  }
+}
+</script>
---
+++ src/components/Switch.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Switch组件</div>
+</template>
+
+<script>
+export default {
+  name: 'Switch'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
