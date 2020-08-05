d52d0744c3631e3c4b8aecc5c58c0979be52fe2b
Lee <326656669@qq.com>
2020-08-05 20:18:53 +0800

    feat : 可以接受 value 和 input 事件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,13 +1,18 @@
 <template>
-  <Switch></Switch>
+  <Switch :value="y" @input="y = $event"></Switch>
 </template>
 
 <script lang="ts">
 import Switch from '../lib/Switch.vue'
+import {ref} from 'vue'
 
 export default {
   name: 'SwitchDemo',
-  components: {Switch}
+  components: {Switch},
+  setup() {
+    const y = ref(true)
+    return {y}
+  }
 }
 </script>
 
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :class="{checked}" @click="toggle">
+  <button :class="{checked:value}" @click="toggle">
     <span></span>
   </button>
 </template>
@@ -9,12 +9,14 @@ import { ref } from 'vue'
 
 export default {
   name: 'Switch',
-  setup() {
-    const checked = ref(false)
+  props: {
+    value: Boolean
+  },
+  setup(props, context) {
     const toggle = () => {
-      checked.value = !checked.value
+      context.emit('input', !props.value) // this.$emit
     }
-    return { checked, toggle }
+    return { toggle }
   }
 }
 </script>
