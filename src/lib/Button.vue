4fefa5cbcb5ef9a771fa98aeb9176648e5332202
Lee <326656669@qq.com>
2020-08-19 19:53:38 +0800

    feat : 添加基础样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,10 +1,7 @@
 <template>
-  <div :size="size">
-    <button v-bind="rest">
-      <slot/>
-    </button>
-  </div>
-
+  <button class="wm-button" :class="`theme-${theme}`">
+    <slot/>
+  </button>
 </template>
 
 <script lang="ts">
@@ -12,18 +9,47 @@ import {ref} from 'vue'
 
 export default {
   name: 'MWButton',
-  inheritAttrs: false,
   props: {
-    size: String,
-    disabled: Boolean
+    theme: {type: String, default: 'button'}
   },
-  setup(props, context) {
-    const {size, ...rest} = context.attrs
-    return {size, rest}
+  setup() {
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+.mw-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  & + & {
+    margin-left: 8px;
+  }
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    border: 0;
+  }
+}
 </style>
