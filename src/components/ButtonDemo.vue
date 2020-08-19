e85220a8beb4c02f51fdc9d06b383707f76e7505
Lee <326656669@qq.com>
2020-08-19 18:19:57 +0800

    feat : 添加button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,10 +1,17 @@
 <template>
   <div>Button组件</div>
+  <h1>示例1</h1>
+  <div>
+    <Button>你好</Button>
+  </div>
 </template>
 
 <script>
+import Button from '../lib/button.vue'
+
 export default {
-  name: 'Button'
+  name: 'Button',
+  components: { Button }
 }
 </script>
 
---
+++ src/lib/button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button>
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import {ref} from 'vue'
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
